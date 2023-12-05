f3056f952a883fbebe7d9c309ae45166f9b23a2d
Gunjan56 <gangwargunjan9@gmail.com>
2023-12-05 17:31:39 +0530

    update

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const form = document.querySelector('.form');
 const firstName = document.querySelector('.fname');
 const lastName = document.querySelector(".lname");
+const fullName = document.querySelector(".fullname")
 const email = document.querySelector('.email');
 const phoneNo = document.querySelector('.phone');
+const error = document.querySelectorAll(".error");
+const input = document.querySelectorAll('input');
 
 form.addEventListener('submit', function(event){
-    event.preventDefault();
+    
     console.log('FirstName: ', firstName.value);
     console.log('LastName: ', lastName.value);
+    console.log('FullName: ', fullName.value);
     console.log('Email: ', email.value);
     console.log('PhoneNo: ', phoneNo.value);
-})
+    event.preventDefault();
+    
+});
+
+
